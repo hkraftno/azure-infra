634d336350e4e835d18a40f636b411fb2d7e5952
Stein Inge Morisbak <stein.inge.morisbak@hkraft.no>
2018-12-14 21:11:56 +0100

    fixed a comment typo in script

--- infrastructure/scripts/create_user.sh
+++ infrastructure/scripts/create_user.sh
@@ -3,6 +3,6 @@
 umask 077
 adduser --quiet --disabled-password --shell /bin/bash --home /home/${1} --gecos '' ${1}
 mkdir /home/${1}/.ssh
-echo "${2}" #> /home/${1}/.ssh/authorized_keys
+echo "${2}" > /home/${1}/.ssh/authorized_keys
 chown -R ${1}:${1} /home/${1}/.ssh
 
